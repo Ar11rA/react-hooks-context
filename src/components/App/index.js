df9b8c213bb0af71bb93c8cbd83d841d9e213bfe
Aritra Ghosh <aritra119.26@gmail.com>
2019-06-07 17:10:28 +0530

    Initial setup: react hooks

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductListUseState from '../ProductListUseState'
+import ProductListUseEffect from '../ProductListUseEffect'
+
+function App() {
+  return (
+  <div>  
+    <ProductListUseState />
+    <ProductListUseEffect />
+  </div>  
+  );
+}
+
+export default App;
---
+++ src/components/ProductListUseState/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+const ProductList = () => { 
+  const [products] = useState([{ id: 1, name: 'Fortnite' }]); 
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (p) => { 
+    const newCartItem = { ...p }; 
+    setCart([...cart, newCartItem]); 
+  }
+
+  return ( 
+    <div> 
+      <h2>Cart items</h2> 
+      {cart.map(item => <div>{item.name}</div>)} 
+     <h2>Products</h2> 
+     {products.map(p => <div onClick={() => addToCart(p)}>{p.name}</div>)} 
+    </div> 
+  ) 
+}
+export default ProductList;
